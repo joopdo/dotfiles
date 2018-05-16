e62b0f0a7fc6c7fdd28f54cc9af798d59e946542
joop <joop@joop.in>
2018-05-16 13:16:10 +0800

    various updates

---
+++ scripts/cron.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+~/.dotfiles/scripts/configure-xrandr.sh && ~/.dotfiles/scripts/keyboard-switch-super.sh
---
+++ scripts/dailycommit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+DATE=`date +%Y-%m-%d`
+/usr/bin/git add /home/joop/syncthing/gtd/
+/usr/bin/git commit -m "commit $DATE" /home/joop/syncthing/gtd/
