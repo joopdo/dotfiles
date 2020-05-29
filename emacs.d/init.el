99ca96027564d30996fd3802049926933a1f167f
J <j>
2020-05-29 09:05:05 +0200

    cleaned up S3 a bit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
 (setq delete-old-versions -1)
@@ -12,9 +19,9 @@
 (remove-hook 'text-mode-hook #'turn-on-auto-fill)
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
 
-(require 'org-bullets)
-(use-package org-bullets
-    :hook (org-mode . org-bullets-mode))
+;(require 'org-bullets)
+;(use-package org-bullets
+;    :hook (org-mode . org-bullets-mode))
 
 ;; find aspell and hunspell automatically
 (cond
@@ -46,7 +53,7 @@
 
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((shell . t)
+ '((sh . t)
  (python . t)
    (emacs-lisp . t)))
 
@@ -62,3 +69,25 @@
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(evil-want-fine-undo t)
+ '(global-linum-mode t)
+ '(global-undo-tree-mode nil)
+ '(inhibit-startup-screen t)
+ '(line-number-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Amazon Ember Mono" :foundry "DAMA" :slant normal :weight normal :height 113 :width normal)))))
--- scripts/configure-xrandr.sh
+++ scripts/configure-xrandr.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -e
 
-PRIMARY="LVDS1"
-EXT1="HDMI1"
-EXT2="VGA-0"
+PRIMARY="eDP-1"
+EXT1="HDMI-1"
+EXT2="HDMI1"
 
 if (xrandr | grep "$EXT1 connected"); then
     xrandr --output $EXT2 --off
-    xrandr --output $PRIMARY --primary --auto --output $EXT1 --auto --right-of $PRIMARY
+    xrandr --output $PRIMARY --primary --rotate normal --auto --output $EXT1 --auto --right-of $PRIMARY
     echo "second screen (DOCK) enabled"
 elif (xrandr | grep "$EXT2 connected"); then
     xrandr --output $EXT2 --off
-    xrandr --output $PRIMARY --primary --auto --output $EXT2 --auto --right-of $PRIMARY
+    xrandr --output $PRIMARY --primary --auto --rotate normal --output $EXT2 --auto --right-of $PRIMARY
     echo "second screen (VGA) enabled"
 else
     xrandr --output $EXT1 --off
     xrandr --output $EXT2 --off
-    xrandr --output $PRIMARY --primary --auto
+    xrandr --output $PRIMARY --primary --rotate normal --auto
     echo "second screen disabled"
 fi
